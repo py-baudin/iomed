7e2750dba4858cf19f8906a69d09c86f1c63398d
Pierre-Yves <py.baudin@institut-myologie.org>
2026-07-06 15:44:34 +0200

    added init

---
+++ src/iomed/__init__.py
@@ -0,0 +1,15 @@
+from . import volume, config, labelfile
+
+volread = volume.read
+volwrite = volume.write
+read_volume = volume.read
+write_volume = volume.write
+tovolume = volume.tovolume
+asvolume = volume.asvolume
+
+read_config = config.read
+write_config = config.write
+
+read_labels = labelfile.read
+write_labels = labelfile.write
+
